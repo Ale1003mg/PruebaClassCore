5128d757516b8f65a3656ec2ea9d83a7f429d8e4
MitoCode <ale1003mg@gmail.com>
2020-04-29 20:07:02 -0600

    Creacion de la clase de bases de datos a ser probada

---
+++ DBGestion/Inventario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBGestion
+{
+    public partial class Inventario
+    {
+        protected static string Conexion = "Data Source=ALEJANDRO\SQLPRUEBAS;Initial Catalog=Gestion;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        protected int ID_Inv { get; set; }
+        protected string Inv_Nombre { get; set; }
+        protected int ID_categoria { get; set; }
+        protected string Descripcion { get; set; }
+        protected int Cantidad { get; set; }
+        protected int Estado { get; set; }
+
+        //mostrar datos
+
+        public int MID_Inv() { return ID_Inv; }
+        public string MInv_Nombre() { return Inv_Nombre; }
+        public int MID_categoria() { return ID_categoria; }
+        public string MDescripcion() { return Descripcion; }
+        public int MCantidad() { return Cantidad; }
+        public int MEstado() { return Estado; }
+
+
+        public void Obtener(int ID,string Nombre,int categoria,string Descrip,int Canti,int Esta) {
+
+            ID_Inv = ID;
+            Inv_Nombre = Nombre;
+            ID_categoria = categoria;
+            Descripcion = Descrip;
+            Cantidad = Canti;
+            Estado = Esta;
+
+    }
+    }
+}
---
+++ DBGestion/SInventario.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DBGestion
+{
+    public partial class SInventario : Inventario
+    {
+
+        SqlConnection cn = new SqlConnection(Conexion);
+        //mostrar
+
+        //Insert
+        public int Insert()
+        {
+            try
+            {
+                SqlCommand sql = new SqlCommand();
+                cn.Open();
+                sql.Connection = cn;
+                sql.CommandType.ToString();
+                sql.CommandText = "insert  Inventario  values(,@Inv_Nombre,@ID_cat,@Descripcion,@Cantidad,@Estado)";
+               // sql.Parameters.Add("@id", SqlDbType.Int);
+               // sql.Parameters["@id"].Value = ID_Inv;
+                sql.Parameters.Add("@Inv_Nombre", SqlDbType.NVarChar);
+                sql.Parameters["@Inv_Nombre"].Value = Inv_Nombre;
+                sql.Parameters.Add("@ID_cat", SqlDbType.Int);
+                sql.Parameters["@ID_cat"].Value = ID_categoria;
+                sql.Parameters.Add("@Descripcion", SqlDbType.NVarChar);
+                sql.Parameters["@Descripcion"].Value = Descripcion;
+                sql.Parameters.Add("@Cantidad", SqlDbType.Int);
+                sql.Parameters["@Cantidad"].Value = Cantidad;
+                sql.Parameters.Add("@Estado", SqlDbType.Int);
+                sql.Parameters["@Estado"].Value = Estado;
+                sql.ExecuteNonQuery();
+                cn.Close();
+                return 2;
+            }
+            catch (Exception)
+            {
+                return 1;
+                throw;
+            }
+        }
+        //Delete
+        public int Delete()
+        {
+            try
+            {
+                SqlCommand sql = new SqlCommand();
+                sql.Connection = cn;
+                sql.CommandType.ToString();
+                sql.CommandText = "DELETE FROM Inventario WHERE ID_Inv=@id";
+                 sql.Parameters.Add("@id", SqlDbType.Int);
+                 sql.Parameters["@id"].Value = ID_Inv;
+                SqlDataAdapter da = new SqlDataAdapter(sql);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                return 2;
+            }
+            catch (Exception)
+            {
+                return 1;
+                throw;
+            }
+        }
+        //Buscar
+        public int Buscar()
+        {
+            try
+            {
+                SqlCommand sql = new SqlCommand();
+                sql.Connection = cn;
+                sql.CommandType.ToString();
+                sql.CommandText = "Select * from Inventario where ID_Inv=@id";
+                sql.Parameters.Add("@id", SqlDbType.Int);
+                sql.Parameters["@id"].Value = ID_Inv;
+                SqlDataAdapter da = new SqlDataAdapter(sql);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+
+                if (ds.Tables[0].Rows.Count>0)
+                {
+                    ID_Inv = Convert.ToInt32( ds.Tables[0].Rows[0][0].ToString());
+                    Inv_Nombre =  ds.Tables[0].Rows[0][1].ToString();
+                    ID_categoria = Convert.ToInt32(ds.Tables[0].Rows[0][2].ToString());
+                    Descripcion = ds.Tables[0].Rows[0][3].ToString();
+                    Cantidad = Convert.ToInt32(ds.Tables[0].Rows[0][4].ToString());
+                    Estado = Convert.ToInt32(ds.Tables[0].Rows[0][5].ToString());
+                    return 2;
+                }
+                else
+                {
+                    return 3;
+                }
+
+               
+            }
+            catch (Exception)
+            {
+                return 1;
+                throw;
+            }
+        }
+        //Update
+        public int Modificar()
+        {
+            try
+            {
+                SqlCommand sql = new SqlCommand();
+                cn.Open();
+                sql.Connection = cn;
+                sql.CommandType.ToString();
+                sql.CommandText = "update  Inventario  set ID_Inv=@id,Onv_Nombre=@Inv_Nombre,ID_Cat=@ID_cat,Descripcion=@Descripcion,cantidad=@Cantidad,Estado=@Estado where ID_Inv=id";
+                sql.Parameters.Add("@id", SqlDbType.Int);
+                sql.Parameters["@id"].Value = ID_Inv;
+                sql.Parameters.Add("@Inv_Nombre", SqlDbType.NVarChar);
+                sql.Parameters["@Inv_Nombre"].Value = Inv_Nombre;
+                sql.Parameters.Add("@ID_cat", SqlDbType.Int);
+                sql.Parameters["@ID_cat"].Value = ID_categoria;
+                sql.Parameters.Add("@Descripcion", SqlDbType.NVarChar);
+                sql.Parameters["@Descripcion"].Value = Descripcion;
+                sql.Parameters.Add("@Cantidad", SqlDbType.Int);
+                sql.Parameters["@Cantidad"].Value = Cantidad;
+                sql.Parameters.Add("@Estado", SqlDbType.Int);
+                sql.Parameters["@Estado"].Value = Estado;
+                sql.ExecuteNonQuery();
+                cn.Close();
+                return 2;
+            }
+            catch (Exception)
+            {
+                return 1;
+                throw;
+            }
+        }
+
+        public void mostrar() {
+            SqlCommand sql = new SqlCommand();
+            sql.Connection = cn;
+            sql.CommandType.ToString();
+            sql.CommandText = "Select * from Inventario";
+
+            SqlDataAdapter da = new SqlDataAdapter(sql);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+        }
+    }
+}
